08a6e18d3194289d71661ecfcde3e9259f43fed7
junjun826-taiiku <junjun.tama@icloud.com>
2026-06-19 15:19:21 +0900

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,14 @@
+export default function Home() {
+  return (
+    <main style={{ padding: 24 }}>
+      <h1>成田山詣行脚 進行管理アプリ</h1>
+      <p>Next.js版の土台です。</p>
+      <ul>
+        <li>ログイン画面へ進む</li>
+        <li>チェックポイント一覧を見る</li>
+        <li>第1班通過時刻を入力する</li>
+      </ul>
+    </main>
+  )
+}
+
